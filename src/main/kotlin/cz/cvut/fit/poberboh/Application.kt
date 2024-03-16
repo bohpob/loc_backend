c523780cbd3190a2f0855d4eee230f0eff4a8606
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2024-03-16 23:54:31 +0100

    implementation of the main application module

--- src/main/kotlin/cz/cvut/fit/poberboh/Application.kt
+++ src/main/kotlin/cz/cvut/fit/poberboh/Application.kt
@@ -1,6 +1,12 @@
 package cz.cvut.fit.poberboh
 
+import cz.cvut.fit.poberboh.data.users.UserEntityDao
+import cz.cvut.fit.poberboh.data.users.UserEntityDaoImpl
+import cz.cvut.fit.poberboh.database.DatabaseSingleton
 import cz.cvut.fit.poberboh.plugins.*
+import cz.cvut.fit.poberboh.security.hashing.SHA256HashingService
+import cz.cvut.fit.poberboh.security.token.JwtTokenService
+import cz.cvut.fit.poberboh.security.token.TokenConfig
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -8,8 +14,24 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
-    configureSerialization()
+    DatabaseSingleton.init()
+
+    val userEntityDao: UserEntityDao = UserEntityDaoImpl()
+    val tokenService = JwtTokenService()
+    val tokenConfig = environment.createTokenConfig()
+    val hashingService = SHA256HashingService()
+
+    configureSecurity(tokenConfig)
+    configureRouting(userEntityDao, hashingService, tokenService, tokenConfig)
     configureMonitoring()
-    configureSecurity()
-    configureRouting()
+    configureSerialization()
+}
+
+fun ApplicationEnvironment.createTokenConfig(): TokenConfig {
+    return TokenConfig(
+        issuer = config.property("jwt.issuer").getString(),
+        audience = config.property("jwt.audience").getString(),
+        expiresIn = config.property("jwt.expiresIn").getString().toLong(),
+        secret = config.property("jwt.secret").getString()
+    )
 }
--- src/main/kotlin/cz/cvut/fit/poberboh/plugins/Monitoring.kt
+++ src/main/kotlin/cz/cvut/fit/poberboh/plugins/Monitoring.kt
@@ -3,7 +3,7 @@ package cz.cvut.fit.poberboh.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
-import org.slf4j.event.*
+import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
